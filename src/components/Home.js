e9d5bc47fd6d4ba107516e8c1357ca347c0e8a4d
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-02-08 13:44:02 +0500

    Create Home component

---
+++ src/components/Home.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <div className='Home'>
+      <div className='showcase'>
+        <img />
+      </div>
+      <div className='features d-flex justify-content-center text-center'>
+        <div className='card'>
+          <div className='card-body'>
+            <div className='card-text'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
+              quis quasi quas, in nihil quidem?
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='card-body'>
+            <div className='card-text'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
+              quis quasi quas, in nihil quidem?
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='card-body'>
+            <div className='card-text'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
+              quis quasi quas, in nihil quidem?
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='card-body'>
+            <div className='card-text'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
+              quis quasi quas, in nihil quidem?
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='card-body'>
+            <div className='card-text'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
+              quis quasi quas, in nihil quidem?
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <div className='card-body'>
+            <div className='card-text'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
+              quis quasi quas, in nihil quidem?
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
