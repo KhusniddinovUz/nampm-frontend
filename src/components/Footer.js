32e5073250f8f2e9dc3af99975694978905a955e
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-02-25 12:41:47 +0500

    change entire footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,52 +1,82 @@
 import { NavLink } from 'react-router-dom';
 import './Footer.scss';
-import tg from '../images/Footer/telegram.svg';
-import fb from '../images/Footer/facebook.svg';
-import insta from '../images/Footer/instagram.svg';
-import tw from '../images/Footer/twitter.svg';
 
 const Footer = () => {
   return (
-    <footer className='Footer text-center pb-3'>
-      <h1 className='mt-3 mb-4'>Presidential School In Namangan</h1>
-      <div className='footer-links d-flex'>
-        <NavLink to='/about'>About</NavLink>
-        <NavLink to='/news'>News</NavLink>
-        <NavLink to='/admission'>Admission</NavLink>
-      </div>
-      <div className='contact'>
-        <ul className='contact-ul'>
-          <li>
-            <i className='fas fa-phone-alt' />
-            <span>+998 99 977 59 65</span>
-          </li>
-          <li>
-            <i className='fas fa-envelope' />
-            <span>khusniddinovuz@gmail.com</span>
-          </li>
-          <li>
-            <i className='fas fa-map-marker-alt' />
-            <span>Namangan shahri, I.Karimov ko'chasi</span>
-          </li>
-          <li>
-            <i className='fas fa-link' />
-            <span>bit.ly/nampmuz</span>
-          </li>
-        </ul>
-      </div>
-      <div className='footer-icons'>
-        <a target='_blank' href='https://t.me/nampmuz'>
-          <img width='60' height='60' src={tg} alt='Telegram' />
-        </a>
-        <a target='_blank' href='https://fb.com/nampmuz'>
-          <img width='60' height='60' src={fb} alt='Facebook' />
-        </a>
-        <a target='_blank' href='https://instagram.com/nampmuz'>
-          <img width='60' height='60' src={insta} alt='Instagram' />
-        </a>
-        <a target='_blank' href='https://twitter.com/nampmuz'>
-          <img width='60' height='60' src={tw} alt='Twitter' />
-        </a>
+    <footer class='footer'>
+      <div class='footer-content'>
+        <div class='footer-section about pr-0'>
+          <h1 className='text-center mb-3'>Presidential School</h1>
+          <p className='p-2'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+            eveniet facilis unde non explicabo expedita sint totam dolorem a
+            eaque.
+          </p>
+          <div className='contact'>
+            <div className='p-1'>
+              <i class='fas fa-phone'></i> &nbsp; 99 XXX XX XX
+            </div>
+            <div className='p-1'>
+              <i class='fas fa-envelope'></i> &nbsp; khusniddinovuz@gmail.com
+            </div>
+            <div className='p-1'>
+              <i class='fas fa-map-marker-alt'></i> &nbsp; Nam City, I.Karimov 1
+            </div>
+          </div>
+        </div>
+
+        <div class='footer-section links'>
+          <h2 className='text-center mb-2'>Quick Links</h2>
+          <ul>
+            <li>
+              <NavLink to='/home'>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to='/about'>About</NavLink>
+            </li>
+            <li>
+              <NavLink to='/news'>News</NavLink>
+            </li>
+            <li>
+              <NavLink to='/admission'>Addmission</NavLink>
+            </li>
+          </ul>
+          <div class='socials'>
+            <a href='#' id='fb' target='_blank'>
+              <i class='fab fa-facebook'></i>
+            </a>
+            <a href='#' id='insta' target='_blank'>
+              <i class='fab fa-instagram'></i>
+            </a>
+            <a href='#' id='twitter' target='_blank'>
+              <i class='fab fa-twitter'></i>
+            </a>
+            <a href='#' id='tube' target='_blank'>
+              <i class='fab fa-youtube'></i>
+            </a>
+          </div>
+        </div>
+
+        <div class='footer-section contact-form'>
+          <h2 className='text-center mb-4'>Contact us</h2>
+          <form className='form-group'>
+            <input
+              type='email'
+              className='contact-input mb-2'
+              placeholder='Email address'
+            />
+            <textarea
+              rows='4'
+              className='contact-input'
+              placeholder='Message'
+            ></textarea>{' '}
+            <br />
+            <button type='submit' class='button'>
+              <i class='fas fa-envelope'></i>
+              Send
+            </button>
+          </form>
+        </div>
       </div>
     </footer>
   );
